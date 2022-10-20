537f0e848d934c770ffe3327948329ca5adc88ea
xTony0x <jabelteton@correo.utl.edu.gt>
2022-10-19 20:40:16 -0600

    pilas y cambios

--- Proyecto_PA/Proyecto_PA/MyForm.h
+++ Proyecto_PA/Proyecto_PA/MyForm.h
@@ -1,5 +1,6 @@
 #pragma once
 #include<stdlib.h>
+#include<stack>
 namespace ProyectoPA {
 
 	using namespace System;
@@ -35,10 +36,14 @@ namespace ProyectoPA {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Button^ button1;
+
 	protected:
-	private: System::Windows::Forms::Label^ label1;
+
 	private: System::Windows::Forms::OpenFileDialog^ openFileDialog1;
+
+
+
+
 
 	private:
 		/// <summary>
@@ -53,29 +58,8 @@ namespace ProyectoPA {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->openFileDialog1 = (gcnew System::Windows::Forms::OpenFileDialog());
 			this->SuspendLayout();
-			// 
-			// button1
-			// 
-			this->button1->Location = System::Drawing::Point(96, 12);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(99, 29);
-			this->button1->TabIndex = 0;
-			this->button1->Text = L"Agregar mapa";
-			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
-			// 
-			// label1
-			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(13, 21);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(77, 13);
-			this->label1->TabIndex = 1;
-			this->label1->Text = L"Ingresar mapa:";
 			// 
 			// openFileDialog1
 			// 
@@ -85,14 +69,11 @@ namespace ProyectoPA {
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(539, 462);
-			this->Controls->Add(this->label1);
-			this->Controls->Add(this->button1);
+			this->ClientSize = System::Drawing::Size(407, 333);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			this->ResumeLayout(false);
-			this->PerformLayout();
 
 		}
 		//hecho por Boris Garzona y Anthony Belteton
@@ -101,13 +82,10 @@ namespace ProyectoPA {
 	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
 	}
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-		System::String^ Text_n;
-		openFileDialog1->Title = "Open Text files";
-		openFileDialog1->ShowDialog();
-		Text_n = openFileDialog1->FileName;
-		String^ fileName = Text_n;
-		MessageBox::Show("trying to open file {0}...", fileName);
-		StreamReader^ din = File::OpenText(fileName);
+		
+	}
+	public: void pilas() {
+
 	}
 	};
 }
